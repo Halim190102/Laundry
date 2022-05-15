80953898d9b4065f2f38db5ffd048bc1c0db837e
Ridwan Halim <190102hr@gmail.com>
2022-05-15 17:05:25 +0700

    Mini Project

--- mini_project/lib/main.dart
+++ mini_project/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
+    return ChangeNotifierProvider<DataManager>(
       create: (context) => DataManager(),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
